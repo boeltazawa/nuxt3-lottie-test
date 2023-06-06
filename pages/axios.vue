6d1e9b38d3d579ed9ae8907f86e0cf2ee5a91d99
BOEL Unno <developer@boel.co.jp>
2023-06-06 10:57:08 +0900

    axiosで取得する方法を検証

---
+++ pages/axios.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { Projects } from "~/types/projects";
+import axios from 'axios'
+definePageMeta({
+    layout: 'page',
+})
+
+
+// const { data } = await useMicroCMSGetListDetail<Projects>({
+//     endpoint: 'projects',
+//     contentId: 'tomamu-wedding',
+//     queries: {
+//         fields: 'subject',
+//     },
+// });
+
+const slug = 'tomamu-wedding';
+
+const { data } = await axios.get(
+      ` https://boel.microcms.io/api/v1/projects/${slug}`,
+      {
+          headers: { 'X-MICROCMS-API-KEY': '9cec95f2379d4b92b808aca78b342376e1d2' }
+      }
+  )
+
+// const detail = data._value
+
+// const { data: details, error }: any = await useFetch(`/api/detail`, {
+//     params: { slug: 'tomamu-wedding' },
+// });
+// const detail = details._value
+
+// let defaultOptions = { animationData: '' }
+// if(detail.subjects.isTitleAnimation) {
+//     defaultOptions = {animationData: detail.subject.animationData.url}
+// }
+// let defaultOptions = { animationData: '' }
+// if(data.subject.isTitleAnimation) {
+//     defaultOptions = {animationData: data.subject.animationData.url}
+// }
+
+const animation = ref(null)
+const handleAnimation = (anim: any) => {
+    animation.value = anim
+    anim.goToAndPlay(0.,true)
+}
+
+</script>
+<template>
+    <div class="content">
+        <LazyHeaderLogo />
+        <!-- <p>animationLink</p>
+        <ul>
+            <li v-for="project in data?.subject" :key="project">
+                {{ project }}
+            </li>
+        </ul> -->
+            
+            <!-- <LazyLottie :options="{animationData: data.subject.animationData.url}" :project="true" @animCreated="handleAnimation" class="projects__ttl-anim" /> -->
+        <template v-if="data?.subject?.isTitleAnimation">
+            <p>animationLink</p>
+            <LazyLottie :options="{animationData: data.subject.animationData.url}" :project="true" @animCreated="handleAnimation" class="projects__ttl-anim" />
+        </template>
+    </div>
+</template>
+<style>
+.content{
+    margin: 0 auto;
+}
+.projects__ttl-anim{
+    display: block;
+    height: 36px;
+    margin-top: 8px;
+}
+</style>
